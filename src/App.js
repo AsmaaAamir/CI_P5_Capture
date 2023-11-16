38cd4062288f12884533818c623470ceb820d255
AsmaaAamir <>
2023-11-16 03:38:37 +0000

    checked if the swtich router was working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+   
 
 function App() {
-  return (
-    <div className="App">
-       <NavBar />
-    </div>
+    return (
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Body}>
+                <Switch>
+                    <Route exact path="/" render={() => <h1>Home Page</h1>} />
+                    <Route exact path="/signin" render={() => <h1>Sign In Page</h1>} />
+                    <Route exact path="/signup" render={() => <h1>Sign Up Page</h1>} />
+                </Switch>
+        </Container>
+        </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container,  Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
 import logo from '../../src/assets/new_logo.png';
 import styles from "../styles/NavBar.module.css";
 
@@ -8,34 +7,34 @@ const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-        <NavLink to="/">
+        <Nav.Link to="/">
             <Navbar.Brand>
                 <img src={logo} alt="logo" height="85"/>
             </Navbar.Brand>
-        </NavLink>
+        </Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
-                <NavLink to="/" className={styles.NavLink} activeClassnAME={styles.Active} >
+                <Nav.Link to="/" className={styles.NavLink} activeClassName={styles.Active} >
                         <i class="fas fa-home">
                             Home
                         </i> 
-                    </NavLink>
-                    <NavLink exact to="/feed" className={styles.NavLink} activeClassnAME={styles.Active} >
+                    </Nav.Link>
+                    <Nav.Link exact to="/feed" className={styles.NavLink} activeClassName={styles.Active} >
                         <i class="fas fa-images">
                             Feed
                         </i>
-                    </NavLink>
-                    <NavLink exact to="/addpost" className={styles.NavLink} activeClassnAME={styles.Active} >
+                    </Nav.Link>
+                    <Nav.Link exact to="/addpost" className={styles.NavLink} activeClassName={styles.Active} >
                         <i class="fas fa-plus-square">
                             Add Post
                         </i>
-                    </NavLink>
-                    <NavLink exact to="/profile" className={styles.NavLink} activeClassnAME={styles.Active} >
+                    </Nav.Link>
+                    <Nav.Link exact to="/profile" className={styles.NavLink} activeClassName={styles.Active} >
                         <i class="fas fa-user">
                             Profile
                         </i>
-                    </NavLink>
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
