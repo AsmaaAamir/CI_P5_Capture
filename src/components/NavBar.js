7a2249f81fc7e4e174be0e030eb010dad2f9219d
AsmaaAamir <>
2023-11-16 06:19:04 +0000

    added sigh out icon and tested o see if user is sign out and it owkrs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,28 +3,68 @@ import { Container,  Nav, Navbar } from "react-bootstrap";
 import logo from '../../src/assets/new_logo.png';
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser, } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
+
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
-    const loggedOutMenu = (
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post("dj-rest-auth/logout/");
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const addPost = (
+        <NavLink to="/addpost" className={styles.NavLink} activeClassName={styles.Active} >
+            <i class="fas fa-plus-square">
+                Add Post
+            </i>
+        </NavLink>
+    );
+
+    const loggedInMenu = (
         <>
-            <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active} >
-               <i class="fas fa-sign-in-alt">
-                    Sign In
+            <NavLink to={`/profiles/${currentUser?.profile_id}`} className={styles.NavLink} >
+                <Avatar src={currentUser?.profile_image} height={45} />
+            </NavLink>
+            <NavLink to="/feed" className={styles.NavLink} activeClassName={styles.Active} >
+                <i class="fas fa-images">
+                    Feed
                 </i>
             </NavLink>
-            <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active} >
-                <i class="fas fa-user-plus">
-                    Sign Up
-                </i> 
+            <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+                <i class="fas fa-sign-out-alt">
+                    Sign Out
+                </i>
             </NavLink>
         </> 
     );
-    const loggedInMenu = <> {currentUser?.username} 
-        
-        </>
-
+    const loggedOutMenu = (
+            <>
+                <NavLink to="/" className={styles.NavLink} activeClassName={styles.Active} >
+                    <i class="fas fa-home">
+                        Home
+                    </i> 
+                </NavLink>
+                <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active} >
+                <i class="fas fa-sign-in-alt">
+                        Sign In
+                    </i>
+                </NavLink>
+                <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active} >
+                    <i class="fas fa-user-plus">
+                        Sign Up
+                    </i> 
+                </NavLink>
+            </> 
+        );
 
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
@@ -34,14 +74,10 @@ const NavBar = () => {
                     <img src={logo} alt="logo" height="85"/>
                 </Navbar.Brand>
             </NavLink>
+            {currentUser && addPost}
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <NavLink to="/" className={styles.NavLink} activeClassName={styles.Active} >
-                        <i class="fas fa-home">
-                            Home
-                        </i> 
-                    </NavLink>
                    {currentUser ? loggedInMenu : loggedOutMenu}
                 </Nav>
             </Navbar.Collapse>
