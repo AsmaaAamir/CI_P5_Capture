2e4bec6adbdb90e42def43f814eb26563ab61f9d
AsmaaAamir <>
2023-11-21 09:51:21 +0000

    added comment section on post page

---
+++ src/pages/comments/AddCommentForm.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, InputGroup } from "react-bootstrap";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+import styles from "../../styles/AddEditCommentForm.module.css";
+
+
+function AddCommentForm(props){
+    const { post, setPost, setComments, 
+        profileImage, profile_id } = props;
+    const [content, setContent] = useState("");
+
+    const handleChange = (e) => {
+    setContent(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const { data } = await axiosRes.post("/comments/", { 
+                content, post, });
+            
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: [data, ...prevComments.results],
+            }));
+            setPost((prevPost) =>({ 
+                results: [ {
+                    ...prevPost.results[0],
+                    comments_count: prevPost.result[0].comments_count + 1,
+                },
+            ],
+        }));
+        setContent("");
+        } catch (err){
+            console.log(err);
+        }
+    };
+    
+    return (
+        <Form className="mt-2" onSubmit={handleSubmit}>
+            <Form.Group>
+                <InputGroup>
+                    <Link tp={`/profiles/${profile_id}`}>
+                        <Avatar src={profileImage}/>
+                    </Link>
+                    <Form.Control
+                        className={styles.Form}
+                        as="textarea"
+                        row={2}
+                        placeholder="Please type your comment..."
+                        value={content}
+                        onChange={handleChange}
+                    />
+                </InputGroup>
+            </Form.Group>
+            <button className={`${styles.Button} btn d-block ml-auto`}
+                disabled={!content.trim()} type="submit">
+                Post
+            </button>
+        </Form>
+    );
+}
+
+export default AddCommentForm;
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -3,10 +3,17 @@ import { Col, Row, Container } from "react-bootstrap";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import AddCommentForm from "../comments/AddCommentForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState ({ results: [] });
+
 
     useEffect(() => {
         const handleMount = async () => {
@@ -27,7 +34,18 @@ function PostPage() {
         <Col className="py-2 p-0 p-lg-2" lg={8}>
             <p> Popular profile for mobile</p>
             <Post {...post.results[0]} setPosts={setPost} postPage />
-            <Container> Comment
+            <Container> 
+                {currentUser ? (
+                    <AddCommentForm
+                    profile_id={currentUser.profile_id}
+                    profileImage={profile_image}
+                    post={id}
+                    setPost={setPost}
+                    setComments={setComments}
+                    />
+                ) : comments.results.length ? (
+                    "Comments"
+                ) : null}
             </Container>
         </Col>
         <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
