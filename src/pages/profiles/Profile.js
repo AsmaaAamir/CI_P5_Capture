fb72cbee2777869273c172b6123fe8e265482b80
AsmaaAamir <>
2023-11-22 09:21:48 +0000

    userd can follow and unfollow from ppopular profile section

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Profile.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import { useSetProfileData, handleFollow, handleUnfollow } from "../../contexts/ProfileDataContext";
 
 const Profile = (props) => { 
     const { profile, mobile, imageSize = 55} = props;
     const { id, following_id, image, owner } = profile;
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username ===  owner;
+    const {handleFollow, handleUnfollow} = useSetProfileData();
 
     return(
         <div className={`my-3 d-flex align-item-center ${mobile && "flex-column"}`}>
@@ -24,11 +26,11 @@ const Profile = (props) => {
             </div>
             <div className={`text-right ${!mobile && "ml-auto"}`}>
                 {!mobile && currentUser && !is_owner && (following_id ? (
-                    <Button className={`${btnStyles.Button} ${btnStyles.Follow}`} onClick={() => {}}>
+                    <Button className={`${btnStyles.Button} ${btnStyles.Follow}`} onClick={() => handleUnfollow(profile)}>
                             Unfollow 
                     </Button>
                 ) : (
-                    <Button className={`${btnStyles.Button} ${btnStyles.Follow}`} onClick={() => {}}>
+                    <Button className={`${btnStyles.Button} ${btnStyles.Follow}`} onClick={() => handleFollow(profile)}>
                             Follow 
                     </Button>
                 ))}
